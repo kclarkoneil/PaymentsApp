abebb543ab0113fa544eb34f38c906a94aa941d8
HighGroundLover <kclarkoneil@yahoo.ca>
2018-08-03 18:39:06 -0700

    Initial Commit

---
+++ PaymentsApp/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  PaymentsApp
+//
+//  Created by Kit Clark-O'Neil on 2018-08-03.
+//  Copyright © 2018 Kit Clark-O'Neil. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
